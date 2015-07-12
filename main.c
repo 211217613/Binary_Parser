10f0889919c3da0d41e02408bfc7db4722aff562
211217613 <rubenavazquez@gmail.com>
2015-07-11 22:52:07 -0500

    Empty c src

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/* My first attempt at doing something cool
+   this program will load source code and just search for vulnerable 
+   functions 
+  */
+int main(int argc, char * argv[]){
+
+}
