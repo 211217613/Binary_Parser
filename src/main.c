e2220b97c4ee0ab1fbb77b997da19e0d7375828d
211217613 <rubenavazquez@gmail.com>
2015-07-17 03:10:36 -0500

    Incremental change

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 /* My first attempt at doing something cool
    this program will load source code and just search for vulnerable 
    functions 
   */
+
+
+void message(){
+	puts("Missing command line argument");
+}  
 int main(int argc, char * argv[]){
+	if(argc > 0 && argc < 2) {
+		message();
+		return -1;
+	}
+
+	FILE *bin = argv[1];
+
 
 }
